cd590e852ac58e1dd32941ac441495a97cc3ca52
Pollyana <projetosdesenvolvimento1@gmail.com>
2026-04-20 23:50:17 -0300

    original

--- playwright/e2e/online.spec.ts
+++ playwright/e2e/online.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
-test('has title', async ({ page }) => {
+test('a webapp deve estar online', async ({ page }) => {
   await page.goto('https://localhost:5173/')
 
   // Expect a title "to contain" a substring.
