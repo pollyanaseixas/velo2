dbc85de0a924a22f6e99ccdbc6d7058b68ad2dbd
Pollyana <projetosdesenvolvimento1@gmail.com>
2026-04-22 15:41:15 -0300

    pedidos

---
+++ playwright/e2e/pedidos.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test'
+///AAA - Preparar, agir, verificar
+
+test('deve consultar um pedido', async ({ page }) => {
+  await page.goto('http://localhost:5173/')
+  //Preparar
+
+  //Chekponit
+  await expect(page.getByTestId('hero-section').getByRole('heading', { name: 'Velô Sprint' })).toBeVisible()
+
+  //Ação - Agir
+  await page.getByRolee('link', { name: 'Consultar Pedido' }).click()
+
+  //checkpoint
+  await expect(page.getByRole('heading')).toContainText  ('Consultar Pedido')
+
+  await page.getByTestId('search-order-id').fill('VLO-123456')
+  await page.getByTestId('search-order-button').click()
+
+  await expect(page.getByTestId('search-order-id')).fill('VLO-123456')
+  await expect(page.getByTestId('search-order-button')).click()
+//verificar
+  await expect(page.getByTestId('order-result-id')).toBeVisible()
+  await expect(page.getByTestId('order-result-id')).toContainText('VLO-123456')
+
+  await expect(page.getByTestId('order-result-status')).toBeVisible()
+  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+
+
+});
